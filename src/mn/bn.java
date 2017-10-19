711ac7c8caf6142cf914e9297542354e737249cd
Parman Zhansaya <parmanzhansaya@gmail.com>
2017-10-19 12:06:07 +0600

    my second commit

--- src/mn/bn.java
+++ src/mn/bn.java
@@ -15,47 +15,22 @@ public class bn extends HttpServlet {
 
         PrintWriter out = response.getWriter();
         String title = "Using GET Method to Read Form Data";
-        String docType=
+        String docType =
                 "<!doctype html>\n";
 
         out.println(docType +
                 "<html>\n" +
-                "<head><title>" + title + "</title></head>\n"+
+                "<head><title>" + title + "</title></head>\n" +
                 "<body bgcolor = \"#f0f0f0\">\n" +
                 "<hl align = \"center\">" + title + "</h1>\n" +
                 "<ul>\n" +
                 " <li><b>First Name</b>: "
-                + request.getParameter( "first_name") + "\n" +
+                + request.getParameter("first_name") + "\n" +
                 "<li><b>Last Name</b>: "
-                +request.getParameter( "last_name") + "\n" +
+                + request.getParameter("last_name") + "\n" +
                 "</ul>\n" +
                 "</body>\n" +
                 "</html>");
     }
-
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-
-        PrintWriter out = resp.getWriter();
-        String title = "Using POST Method to Read Form Data";
-        String docType=
-                "<!doctype html>\n";
-
-        out.println(docType +
-                "<html>\n" +
-                "<head><title>" + title + "</title></head>\n"+
-                "<body bgcolor = \"#f0f0f0\">\n" +
-                "<hl align = \"center\">" + title + "</h1>\n" +
-                "<ul>\n" +
-                " <li><b>First Name</b>: "
-                + req.getParameter( "first_name") + "\n" +
-                "<li><b>Last Name</b>: "
-                +req.getParameter( "last_name") + "\n" +
-                "</ul>\n" +
-                "</body>\n" +
-                "</html>");
-    }
-
 }
+
