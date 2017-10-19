d5d13739964723868d7b610c88e4678854c4b735
Parman Zhansaya <parmanzhansaya@gmail.com>
2017-10-19 11:24:20 +0600

    my first commit

---
+++ src/mn/bn.java
@@ -0,0 +1,61 @@
+package mn;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class bn extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        String title = "Using GET Method to Read Form Data";
+        String docType=
+                "<!doctype html>\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n"+
+                "<body bgcolor = \"#f0f0f0\">\n" +
+                "<hl align = \"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                " <li><b>First Name</b>: "
+                + request.getParameter( "first_name") + "\n" +
+                "<li><b>Last Name</b>: "
+                +request.getParameter( "last_name") + "\n" +
+                "</ul>\n" +
+                "</body>\n" +
+                "</html>");
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        String title = "Using POST Method to Read Form Data";
+        String docType=
+                "<!doctype html>\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n"+
+                "<body bgcolor = \"#f0f0f0\">\n" +
+                "<hl align = \"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                " <li><b>First Name</b>: "
+                + req.getParameter( "first_name") + "\n" +
+                "<li><b>Last Name</b>: "
+                +req.getParameter( "last_name") + "\n" +
+                "</ul>\n" +
+                "</body>\n" +
+                "</html>");
+    }
+
+}
